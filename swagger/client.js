f6c6ee9b8d223213869c85b8ebbdbf5f69791e10
giplash <anashkinviktar@gmail.com>
2018-05-31 16:22:28 +0300

    add swagger

---
+++ swagger/client.js
@@ -0,0 +1,232 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import * as request from "superagent";
+import {
+    SuperAgentStatic
+} from "superagent";
+
+type CallbackHandler = (err: any, res ? : request.Response) => void;
+type Product = {
+    'name' ? : string
+
+    'description' ? : string
+
+    'price' ? : number
+
+    'data' ? : number
+
+    'image' ? : string
+
+    'music' ? : string
+
+    'instruments' ? : string
+
+    'costumes' ? : string
+
+};
+type Error = {
+    'message': string
+
+};
+
+type Logger = {
+    log: (line: string) => any
+};
+
+/**
+ * 
+ * @class ProductService
+ * @param {(string)} [domainOrOptions] - The project domain.
+ */
+export default class ProductService {
+
+    private domain: string = "http://localhost:10010/api/v1";
+    private errorHandlers: CallbackHandler[] = [];
+
+    constructor(domain ? : string, private logger ? : Logger) {
+        if (domain) {
+            this.domain = domain;
+        }
+    }
+
+    getDomain() {
+        return this.domain;
+    }
+
+    addErrorHandler(handler: CallbackHandler) {
+        this.errorHandlers.push(handler);
+    }
+
+    private request(method: string, url: string, body: any, headers: any, queryParameters: any, form: any, reject: CallbackHandler, resolve: CallbackHandler) {
+        if (this.logger) {
+            this.logger.log(`Call ${method} ${url}`);
+        }
+
+        let req = (request as SuperAgentStatic)(method, url).query(queryParameters);
+
+        Object.keys(headers).forEach(key => {
+            req.set(key, headers[key]);
+        });
+
+        if (body) {
+            req.send(body);
+        }
+
+        if (typeof(body) === 'object' && !(body.constructor.name === 'Buffer')) {
+            req.set('Content-Type', 'application/json');
+        }
+
+        if (Object.keys(form).length > 0) {
+            req.type('form');
+            req.send(form);
+        }
+
+        req.end((error, response) => {
+            if (error || !response.ok) {
+                reject(error);
+                this.errorHandlers.forEach(handler => handler(error));
+            } else {
+                resolve(response);
+            }
+        });
+    }
+
+    getHeroesURL(parameters: {
+        'name' ? : string,
+        'pageSize' ? : number,
+        'pageNumber' ? : number,
+        $queryParameters ? : any,
+        $domain ? : string
+    }): string {
+        let queryParameters: any = {};
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/heroes';
+        if (parameters['name'] !== undefined) {
+            queryParameters['name'] = parameters['name'];
+        }
+
+        if (parameters['pageSize'] !== undefined) {
+            queryParameters['pageSize'] = parameters['pageSize'];
+        }
+
+        if (parameters['pageNumber'] !== undefined) {
+            queryParameters['pageNumber'] = parameters['pageNumber'];
+        }
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+            });
+        }
+
+        let keys = Object.keys(queryParameters);
+        return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '');
+    }
+
+    /**
+     * List of heroes
+     * @method
+     * @name ProductService#getHeroes
+     * @param {string} name - The name of heroes on the page
+     * @param {number} pageSize - Number of products returned
+     * @param {number} pageNumber - Page number
+     */
+    getHeroes(parameters: {
+        'name' ? : string,
+        'pageSize' ? : number,
+        'pageNumber' ? : number,
+        $queryParameters ? : any,
+        $domain ? : string
+    }): Promise < request.Response > {
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/heroes';
+        let body: any;
+        let queryParameters: any = {};
+        let headers: any = {};
+        let form: any = {};
+        return new Promise((resolve, reject) => {
+            headers['Accept'] = 'application/json';
+            headers['Content-Type'] = 'application/json';
+
+            if (parameters['name'] !== undefined) {
+                queryParameters['name'] = parameters['name'];
+            }
+
+            if (parameters['pageSize'] !== undefined) {
+                queryParameters['pageSize'] = parameters['pageSize'];
+            }
+
+            if (parameters['pageNumber'] !== undefined) {
+                queryParameters['pageNumber'] = parameters['pageNumber'];
+            }
+
+            if (parameters.$queryParameters) {
+                Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                    queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+                });
+            }
+
+            this.request('GET', domain + path, body, headers, queryParameters, form, reject, resolve);
+        });
+    }
+
+    getHeroURL(parameters: {
+        'heroId': string,
+        $queryParameters ? : any,
+        $domain ? : string
+    }): string {
+        let queryParameters: any = {};
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/hero/{heroId}';
+
+        path = path.replace('{heroId}', `${parameters['heroId']}`);
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+            });
+        }
+
+        let keys = Object.keys(queryParameters);
+        return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '');
+    }
+
+    /**
+     * Single product for its ID
+     * @method
+     * @name ProductService#getHero
+     * @param {string} heroId - The hero's id
+     */
+    getHero(parameters: {
+        'heroId': string,
+        $queryParameters ? : any,
+        $domain ? : string
+    }): Promise < request.Response > {
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/hero/{heroId}';
+        let body: any;
+        let queryParameters: any = {};
+        let headers: any = {};
+        let form: any = {};
+        return new Promise((resolve, reject) => {
+            headers['Accept'] = 'application/json';
+            headers['Content-Type'] = 'application/json';
+
+            path = path.replace('{heroId}', `${parameters['heroId']}`);
+
+            if (parameters['heroId'] === undefined) {
+                reject(new Error('Missing required  parameter: heroId'));
+                return;
+            }
+
+            if (parameters.$queryParameters) {
+                Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                    queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+                });
+            }
+
+            this.request('GET', domain + path, body, headers, queryParameters, form, reject, resolve);
+        });
+    }
+
+}
